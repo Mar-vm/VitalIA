bc82d98333d0c5eca74a01e2eaa210442f9e6f05
LuisAlbertoOC <143580392+LuisAlbertoOC@users.noreply.github.com>
2025-04-07 23:03:01 -0600

    Create clonar_firestore.py
    Script para clonar la base de datos principal y hacer los test en la clonada

---
+++ scripts/clonar_firestore.py
@@ -0,0 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# ---------- Proyecto Origen ----------
+cred_origen = credentials.Certificate("vitalia.json")
+app_origen = firebase_admin.initialize_app(cred_origen, name='origen')
+db_origen = firestore.client(app_origen)
+
+# ---------- Proyecto Clon ----------
+cred_clon = credentials.Certificate("vitalia-test.json")
+app_clon = firebase_admin.initialize_app(cred_clon, name='clon')
+db_clon = firestore.client(app_clon)
+
+# ---------- Clonar todas las colecciones ----------
+def clonar_coleccion(coleccion):
+    print(f"Clonando colección: {coleccion}")
+    docs = db_origen.collection(coleccion).stream()
+    for doc in docs:
+        db_clon.collection(coleccion).document(doc.id).set(doc.to_dict())
+        print(f"✔ Copiado: {doc.id}")
+
+# Lista de colecciones que quieres clonar
+colecciones = ['afecciones','diagnosticos','historial','imagenes','usuarios']  # cámbialo según tu base
+
+for col in colecciones:
+    clonar_coleccion(col)
+
+print("✅ Clonación completada.")
