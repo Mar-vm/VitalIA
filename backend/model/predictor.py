d5285e8b6b7394b20e7cb91022022d153e555743
Mariana Molina Villanueva <marianamvillanw@gmail.com>
2025-04-07 20:52:09 -0600

    Update predictor.py

--- backend/model/predictor.py
+++ backend/model/predictor.py
@@ -15,7 +15,7 @@ with open(CLASSES_PATH, "r") as f:
 # === Preparar modelo EfficientNet ===
 model = models.efficientnet_b0(weights=None)
 #model.classifier[1] = nn.Linear(model.classifier[1].in_features, len(class_names))
-MODEL_PATH = 'backend/model/efficientnet_b0_rwightman-7f5810bc.pth'
+MODEL_PATH = 'backend/model/modelo_efficientnet_skin_cancer.pth'
 model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
 model.eval()
 
