06c60dbe90504a4e7acbd0e23ce049d70957642a
LuisAlbertoOC <143580392+LuisAlbertoOC@users.noreply.github.com>
2025-05-06 01:24:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,29 @@ import base64
 from PIL import Image
 import io
 import torch
-from torchvision import transforms
+from torchvision import transforms, models
 
 app = Flask(__name__)
 
-# Cargar modelo
-model = torch.load("modelo_efficientnet_skin_cancer.pth", map_location=torch.device('cpu'))
+# Reconstruir arquitectura del modelo (EfficientNet B0)
+model = models.efficientnet_b0(pretrained=False)
+
+# Ajustar la última capa (para 7 clases)
+model.classifier[1] = torch.nn.Linear(in_features=1280, out_features=7)
+
+# Cargar los pesos (state_dict)
+state_dict = torch.load("modelo_efficientnet_skin_cancer.pth", map_location=torch.device('cpu'))
+model.load_state_dict(state_dict)
 model.eval()
 
-# Transformaciones
+# Transformaciones para la imagen
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Clases
+# Clases del modelo
 classes = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']
 
 @app.route('/')
